561fe39bbd2450d19532304fe287b03d5f612865
dpankratov <dpankratov06@yandex.ru>
2023-03-29 13:06:17 +0300

    partioal css for first window

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import threading
 app = Flask(__name__)
 frame_r = 0
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host="0.0.0.0", port=5000)
+    
